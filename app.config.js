2c64a2db16618b0fa53073df00158817476c95f1
sung5148 <nea7267@gmail.com>
2026-06-26 11:14:59 +0900

    app.config.js - 새 Expo 설정 파일 추가 app.json - 삭제됨/app.config.js로 대체 eas.json - EAS 설정 파일 추가 package-lock.json - 패키지 잠금 파일 수정 package.json - scripts/패키지 설정 수정

---
+++ app.config.js
@@ -0,0 +1,72 @@
+export default {
+  expo: {
+    name: "documate-app",
+    slug: "documate-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "documateapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      package: "com.samgim.documate",
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "expo-secure-store",
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/images/android-icon-monochrome.png",
+          color: "#1565C0",
+          defaultChannel: "default",
+        },
+      ],
+      "@react-native-firebase/app",
+      "@react-native-firebase/messaging",
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+  eas: {
+    projectId: "dac4068d-32fe-4a44-977e-2a7f19c5d93c",
+  },
+},
+  },
+};
